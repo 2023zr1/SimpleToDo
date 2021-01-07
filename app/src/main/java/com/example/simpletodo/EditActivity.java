36d155d9ce34b4325d7b95ca5b5546ad36d0e475
2023zr1 <shreya.m.2023@gmail.com>
2021-01-07 00:17:35 -0800

    Initial commit

---
+++ app/src/main/java/com/example/simpletodo/EditActivity.java
@@ -0,0 +1,54 @@
+package com.example.simpletodo;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+
+public class EditActivity extends AppCompatActivity {
+
+    EditText etItem;
+    Button btnSave;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        etItem = findViewById(R.id.etItem);
+        btnSave = findViewById(R.id.btnSave);
+
+        getSupportActionBar().setTitle("Edit item");
+
+        etItem.setText(getIntent().getStringExtra(MainActivity.KEY_ITEM_TEXT));
+
+        // When the user is done editing, they click the save button
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an intent which will contain the results
+                Intent intent = new Intent();
+
+                // Pass the data (results of editing)
+                intent.putExtra(MainActivity.KEY_ITEM_TEXT, etItem.getText().toString());
+                intent.putExtra(MainActivity.KEY_ITEM_POSITION, getIntent().getExtras().getInt(MainActivity.KEY_ITEM_POSITION));
+
+                // Set the result of the intent
+                setResult(RESULT_OK, intent);
+
+                // Finish activity, close the screen and go back
+                finish();
+            }
+        });
+    }
+}
